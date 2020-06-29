d46b65d1d354d7a626f5a25c70a23fbfc2278d33
Nüssli Desktop <johannesrebling@gmail.com>
2020-06-29 18:55:07 +0200

    fire glowing

--- src/glowl.cpp
+++ src/glowl.cpp
@@ -22,14 +22,19 @@ FASTLED_USING_NAMESPACE
 #define DATA_PIN 12
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
-#define NUM_LEDS 135 // 144 - 8, 8 are in the eyes ;-), nee to add those back later
-#define BRIGHTNESS 64 // [0 - 256]
+#define NUM_LEDS 54 // 144 - 8, 8 are in the eyes ;-), nee to add those back later
+#define BRIGHTNESS 255 // [0 - 256]
 CRGB leds[NUM_LEDS]; // contains led info, we set this first, then we call led show
+bool gReverseDirection = false;
+CRGBPalette16 gPal;
+#define COOLING  55
+#define SPARKING 120
 
 // LED control function declarations
 void setup_leds();
 void set_led_status(uint8_t status);
 void pulse_leds(uint8_t nPulses, uint8_t pulseSpeed);
+void Fire2012WithPalette();
 
 // setup time ------------------------------------------------------------------
 #include <time.h>
@@ -104,6 +109,9 @@ void setup()
     u8g2.sendBuffer();
     delay(500);
   }
+
+  gPal = HeatColors_p;
+
 }
 
 void loop()
@@ -131,6 +139,10 @@ void loop()
     u8g2.print(ctime(&now)); 
     u8g2.sendBuffer();
   }
+
+  // random16_add_entropy(random());
+  Fire2012WithPalette(); // run simulation frame, using palette colors
+  FastLED.show(); // display this frame
 
 }
 
@@ -206,4 +218,41 @@ void set_led_status(uint8_t status)
     FastLED.show();
     break;
   }
+}
+
+void Fire2012WithPalette()
+{
+// Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+    for( int i = 0; i < NUM_LEDS; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+  
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
+    
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if( random8() < SPARKING ) {
+      int y = random8(7);
+      heat[y] = qadd8( heat[y], random8(160,255) );
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < NUM_LEDS; j++) {
+      // Scale the heat value from 0-255 down to 0-240
+      // for best results with color palettes.
+      byte colorindex = scale8( heat[j], 240);
+      CRGB color = ColorFromPalette( gPal, colorindex);
+      int pixelnumber;
+      if( gReverseDirection ) {
+        pixelnumber = (NUM_LEDS-1) - j;
+      } else {
+        pixelnumber = j;
+      }
+      leds[pixelnumber] = color;
+    }
 }
